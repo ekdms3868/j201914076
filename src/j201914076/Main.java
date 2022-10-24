015e017ea498e71c8f85538d061e2df7fac2feee
daeun <ehsjt3868@naver.com>
2022-10-24 16:45:27 +0900

    Main.java 수정2

--- src/j201914076/Main.java
+++ src/j201914076/Main.java
@@ -1,9 +1,12 @@
 package j201914076;
 
 public class Main {
+
+    static void say(String s) {
+        System.out.println(s);
+    }
     
     public static void main(String[] args) {
-        String s = "hello world";
-        System.out.println(s);
+        say("hello world");
     }
 }
